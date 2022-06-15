6fed218e147a75a9544a31bfbbd0a8d024c9b632
Mischka Booley <mischka@gearbox.com.au>
2022-06-15 16:10:58 +1000

    complete task lister lite challenge lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  //assign necessary DOM elements to variables
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  //ul where tasks will be appended to in the DOM
+  const newTaskUnorderedList = document.getElementById("tasks");
+
+  const createNewTask = event => {
+    //stop form from trying to submit
+    event.preventDefault();
+
+    const newTask = document.createElement("li");
+    newTask.innerText = newTaskDescription.value;
+    newTaskUnorderedList.appendChild(newTask);
+    event.target.reset();
+  };
+
+  //attach event listeners
+  newTaskForm.addEventListener("submit", createNewTask);
+
 });
+
